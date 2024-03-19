ee7bdd3218917600f52b2f9760bb234115dc4c83
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 13:05:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Predict and Run Example 1
+#The code will display on the console With the name "Andy".
+
+firstName = "Andy"
+
+print(firstName)
+
+#Predict and Run Example 2
+ # The code will display "Andy Colley"
+
+lastName = "Colley"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+#Predict and Run Example 3
+ # This will display "Hello Andy. your full name is Andy Colley. "
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
+
